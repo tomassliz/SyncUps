8c507e4630d5480c581463281d5103bff328aeef
Tomas Sliz <3275022+tomassliz@users.noreply.github.com>
2024-05-10 14:18:48 +0200

    Add new iOS project

---
+++ SyncUps/SyncUpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncUpsApp.swift
+//  SyncUps
+//
+//  Created by Tomas Sliz on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SyncUpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
