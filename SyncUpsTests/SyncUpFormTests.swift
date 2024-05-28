68e74d621b536cf907d98401fd4d5983fd1882eb
Tomas Sliz <3275022+tomassliz@users.noreply.github.com>
2024-05-28 16:46:32 +0200

    Test attendee deletion in SyncUp form

---
+++ SyncUpsTests/SyncUpFormTests.swift
@@ -0,0 +1,50 @@
+import ComposableArchitecture
+import XCTest
+
+@testable import SyncUps
+
+final class SyncUpFormTests: XCTestCase {
+// TODO: This test is broken in the tutorial, fix when it's resolved.
+//  @MainActor
+//  func testRemoveAttendee() async {
+//    let store = TestStore(
+//      initialState: SyncUpForm.State(
+//        syncUp: SyncUp(
+//          id: SyncUp.ID(),
+//          attendees: [
+//            Attendee(id: Attendee.ID()),
+//            Attendee(id: Attendee.ID())
+//          ]
+//        )
+//      )
+//    ) {
+//      SyncUpForm()
+//    }
+//
+//    await store.send(.onDeleteAttendees([0])) {
+//      $0.syncUp.attendees.removeFirst()
+//    }
+//  }
+
+  @MainActor
+  func testRemoveFocusedAttendee() async {
+    let attendee1 = Attendee(id: Attendee.ID())
+    let attendee2 = Attendee(id: Attendee.ID())
+    let store = TestStore(
+      initialState: SyncUpForm.State(
+        focus: .attendee(attendee1.id),
+        syncUp: SyncUp(
+          id: SyncUp.ID(),
+          attendees: [attendee1, attendee2]
+        )
+      )
+    ) {
+        SyncUpForm()
+    }
+
+    await store.send(.onDeleteAttendees([0])) {
+      $0.focus = .attendee(attendee2.id)
+      $0.syncUp.attendees = [attendee2]
+    }
+  }
+}
